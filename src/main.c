61771f17d3caddaba7dac25e0728684cb7c985a9
Ignacio Mercado <igmercado46@gmail.com>
2025-01-13 19:32:10 -0300

    now timer works

--- src/main.c
+++ src/main.c
@@ -1,52 +1,57 @@
 #include "raylib.h"
-
+#include <unistd.h>
 #include "resource_dir.h"
 
-int main ()
-{
-	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-	const int SCREEN_HEIGHT = 800;
-	const int SCREEN_WIDTH = 600;
-	int timeInSeconds = 100;
-	char timeInSecondsStringified[50];
+int main() {
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
-
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bomb timer");
-
-	SearchAndSetResourceDir("resources");
-
-	Texture2D bomb = LoadTexture("bomb.png");
+    const int SCREEN_WIDTH = 600;
+    const int SCREEN_HEIGHT = 800;
+    int timeInSeconds = 60;
+    float timer = 1.0f; 
+    char timeInSecondsStringified[50];
 	
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Bomb Timer");
+    SearchAndSetResourceDir("resources");
+
+
+    Font font = LoadFont("resources/fonts/pixelplay.png");
+    Texture2D bombLightOff = LoadTexture("bomb_light_off.png");
+    Texture2D bombLightOn = LoadTexture("bomb_light_on.png");	
+    
 	while (!WindowShouldClose()) {
+        if (timeInSeconds > 0) {
+            timer -= GetFrameTime();
+            if (timer <= 0) {
+                timeInSeconds--;
+                timer = 1.0f;
+            }
+        }
 
-		BeginDrawing();
+        BeginDrawing();
 
-		if (timeInSeconds == -1) {
-			// drawing time setup screen			
-			ClearBackground(BLACK);
-
-			DrawText("Bomb Timer", 0, 0, 20, WHITE);
-
-			DrawTexture(bomb, SCREEN_HEIGHT/2 - bomb.height/2 , SCREEN_WIDTH/2 - bomb.width/2, WHITE);
-		} else {
-			// drawing time countdown screen
-			ClearBackground(BLACK);
-
-			snprintf(timeInSecondsStringified, sizeof(timeInSecondsStringified), "TIME LEFT IN SEC: %d", timeInSeconds);
-
-			DrawText(timeInSecondsStringified, 0, 0, 20, WHITE);
-
-			DrawTexture(bomb, SCREEN_HEIGHT/2 - bomb.height/2 , SCREEN_WIDTH/2 - bomb.width/2, WHITE);
+        if (timeInSeconds <= 0) {
+            ClearBackground(BLACK);
+            DrawText("TIME IS UP!", SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2, 20, RED);
+        } else {
+            ClearBackground(BLACK);
+			if (timeInSeconds % 2 == 0) {
+				DrawTexture(bombLightOff, SCREEN_WIDTH / 2 - bombLightOff.width / 2, SCREEN_HEIGHT / 2 - bombLightOff.height / 2, WHITE);
+			} else {
+				DrawTexture(bombLightOn, SCREEN_WIDTH / 2 - bombLightOff.width / 2, SCREEN_HEIGHT / 2 - bombLightOff.height / 2, WHITE); 	
+			}
+            snprintf(timeInSecondsStringified, 50, "%d", timeInSeconds);
+    		DrawText(timeInSecondsStringified, (SCREEN_WIDTH / 2 - bombLightOff.width / 2) + 70, (SCREEN_HEIGHT / 2 - bombLightOff.height / 2) + 70, 18, BLACK);
+			
 		}
-		
-		EndDrawing();
-	}
 
-	// cleanup
-	UnloadTexture(bomb);
+        EndDrawing();
+    }
 
-	// destroy the window and cleanup the OpenGL context
-	CloseWindow();
-	return 0;
+    UnloadTexture(bombLightOff);
+    CloseWindow();
+
+    return 0;
 }
